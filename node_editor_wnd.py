05d08c25345dca4118f4e8daff958cd10b1a86a7
i.zuykov <i.zuykov@souzmult.ru>
2022-04-21 22:58:44 +0300

    lesson 1 started view scene and grid background

---
+++ node_editor_wnd.py
@@ -0,0 +1,26 @@
+from PyQt5.QtWidgets import *
+from node_graphics_scene import QDMGraphicsScene
+
+
+class NodeEditorWnd(QWidget):
+    def __init__(self, parent=None):
+        super(NodeEditorWnd, self).__init__(parent)
+
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(200, 200, 800, 600)
+        self.layout = QVBoxLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(self.layout)
+
+        # create graphics scene
+        self.grScene = QDMGraphicsScene()
+
+        # create graphics view
+        self.view = QGraphicsView(self)
+        self.view.setScene(self.grScene)
+        self.layout.addWidget(self.view)
+
+        self.setWindowTitle("Node Editor")
+        self.show()
---
+++ node_graphics_scene.py
@@ -0,0 +1,66 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from math import floor, ceil
+
+
+class QDMGraphicsScene(QGraphicsScene):
+    def __init__(self, parent=None):
+        super(QDMGraphicsScene, self).__init__(parent)
+
+        # settings
+        self.gridSize = 10
+        self.gridSquares = 5
+
+        self._color_background = QColor("#393939")
+        self._color_light = QColor("#2f2f2f")
+        self._color_dark = QColor("#292929")
+
+        self._pen_light = QPen(self._color_light)
+        self._pen_light.setWidth(1)
+
+        self._pen_dark = QPen(self._color_dark)
+        self._pen_dark.setWidth(2)
+
+        self.scene_width, self.scene_height = 64000, 64000
+
+        self.setSceneRect(
+            -self.scene_width // 2,
+            self.scene_height // 2,
+            self.scene_width,
+            self.scene_height,
+        )
+
+        self.setBackgroundBrush(self._color_background)
+
+    def drawBackground(self, painter: QPainter, rect: QRectF):
+        super(QDMGraphicsScene, self).drawBackground(painter, rect)
+
+        # here we will create our grid
+        left = int(floor(rect.left()))
+        right = int(ceil(rect.right()))
+        top = int(floor(rect.top()))
+        bottom = int(ceil(rect.bottom()))
+
+        first_left = left - (left % self.gridSize)
+        first_top = top - (top % self.gridSize)
+
+        # compute all lines to be drawn
+        lines_light, lines_dark = [], []
+        for x in range(first_left, right, self.gridSize):
+            if x % (self.gridSize * self.gridSquares) == 0:
+                lines_dark.append(QLine(x, top, x, bottom))
+            else:
+                lines_light.append(QLine(x, top, x, bottom))
+
+        for y in range(first_top, bottom, self.gridSize):
+            if y % (self.gridSize * self.gridSquares) == 0:
+                lines_dark.append(QLine(left, y, right, y))
+            else:
+                lines_light.append(QLine(left, y, right, y))
+
+        # draw lines
+        painter.setPen(self._pen_light)
+        painter.drawLines(*lines_light)
+        painter.setPen(self._pen_dark)
+        painter.drawLines(*lines_dark)
